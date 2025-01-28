593a987d87c3e7762b6cc0f727ba8e3535bb3e8c
404 <sametalpdeger9@gmail.com>
2025-01-28 17:40:22 +0300

    M: Modified positions

--- src/app/_components/AboutMeSection/index.tsx
+++ src/app/_components/AboutMeSection/index.tsx
@@ -16,16 +16,16 @@ export const AboutMeSection = () => {
     <div className="flex w-full flex-col items-center px-2 py-2 animate-fade">
       <div className="flex flex-col gap-3 max-w-[40rem] items-start">
         <FirstPart />
-        <div className="flex flex-col 1 gap-4 ">
+        <div className="flex flex-col gap-4 ">
           <div className='flex flex-col gap-10 rounded-md  bg-gradient-to-b from-[#00000072] to-[#0000003f] text-[0.9rem] leading-5 text-[rgba(255,255,255,0.7)] shadow-[0_0px_15px_3px_rgba(0,0,0,0.2)]'>
             <Links />
 
-            <div className="prose prose-headings:text-[#ffffffc0] p-3 prose-strong:text-[#ffffffc0] prose-p:text-[#ffffffa0] prose-li:text-[#ffffffa0]">
+            <div className="prose prose-headings:text-[#ffffffc0] px-3 pt-3 pb-6  prose-strong:text-[#ffffffc0] prose-p:text-[#ffffffa0] prose-li:text-[#ffffffa0]">
               <Content />
 
 
               <div className='flex flex-col gap-2 items-center'>
-                <h2>Technologies & Services I Use</h2>
+                <h2 className='text-center w-[90%]'>Technologies & Services I Use</h2>
                 <div className='gap-2 flex flex-row flex-wrap justify-center'>
                   <SkillIcon
                     icon={<Svg src='typescript' className='w-10 h-10' alt='TypeScript logo' />}
