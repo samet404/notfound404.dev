9621ab1dfd87f7517a7ab976227ce83258e4a2c8
404 <sametalpdeger9@gmail.com>
2025-01-28 16:48:46 +0300

    M: Modified colors and positions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
         style={{
           backgroundColor: 'hsla(201, 100%, 50%, 1)',
           backgroundImage:
-            'radial-gradient(at 40% 20%, hsla(188,100%,74%,1) 0px, transparent 50%),radial-gradient(at 80% 0%, hsla(189,100%,56%,1) 0px, transparent 50%),radial-gradient(at 0% 100%, hsla(309,100%,75%,1) 0px, transparent 50%),radial-gradient(at 64% 4%, hsla(61,100%,76%,1) 0px, transparent 50%),radial-gradient(at 0% 100%, hsla(178,100%,50%,1) 0px, transparent 50%),radial-gradient(at 100% 95%, hsla(200,100%,70%,1) 0px, transparent 50%),radial-gradient(at 0% 0%, hsla(113,100%,79%,1) 0px, transparent 50%)',
+            'radial-gradient(at 40% 20%, hsla(188,100%,74%,1) 0px, transparent 50%),radial-gradient(at 80% 0%, hsla(189,100%,56%,1) 0px, transparent 50%),radial-gradient(at 0% 100%, hsla(153, 100%, 75%, 1) 0px, transparent 50%),radial-gradient(at 64% 4%, hsla(150, 100%, 76%, 1) 0px, transparent 50%),radial-gradient(at 0% 100%, hsla(178,100%,50%,1) 0px, transparent 50%),radial-gradient(at 100% 95%, hsla(200,100%,70%,1) 0px, transparent 50%),radial-gradient(at 0% 0%, hsla(113,100%,79%,1) 0px, transparent 50%)',
           scrollbarWidth: 'none',
         }}
         className="flex w-full flex-col gap-7 justify-center "
