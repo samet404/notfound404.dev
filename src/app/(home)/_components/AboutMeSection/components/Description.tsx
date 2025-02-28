abd30a4c838112f7dee733c5c08ed8e9c08dbc23
404 <sametalpdeger9@gmail.com>
2025-02-28 20:07:03 +0300

    Fix spelling

--- src/app/(home)/_components/AboutMeSection/components/Description.tsx
+++ src/app/(home)/_components/AboutMeSection/components/Description.tsx
@@ -1,6 +1,6 @@
 export const Description = () => {
     return (
-        <p>Hello, I'm a 18 yo self - taught full - stack web and native android developer from Turkey.I'm having fun building problem solving, performant and secure applications.I have been working myself on my projects as a software engineer for over 3 years now.I have so many hobbies like reading, writing, watching movies, and playing games.
+        <p>Hello to my personal website, I'm a 18 yo self-taught full-stack Web and Native Android developer from Turkey. I'm having fun building problem solving, performant and secure applications. I've been working myself on my projects as a software engineer for over 3 years now. I have so many hobbies like reading, writing, watching movies, and playing games.
         </p >
     )
 }
