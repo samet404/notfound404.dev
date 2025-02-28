44132e28cebd8a4b6668a1f14f974fc9812a3f6a
404 <sametalpdeger9@gmail.com>
2025-02-28 22:09:27 +0300

    add: react scan
    add react scan
    
    add env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,10 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="en">
       <body className={`${outfit.className} relative selection:bg-[#ffffff54]`}>
+        {process.env.NODE_ENV === 'development' && <script
+          crossOrigin="anonymous"
+          src="//unpkg.com/react-scan/dist/auto.global.js"
+        />}
         {children}
         <Nav />
       </body>
