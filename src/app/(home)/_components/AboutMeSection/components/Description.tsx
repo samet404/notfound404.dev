cb96b982fed003d223cc2b952108e4f15fc887f8
404 <sametalpdeger9@gmail.com>
2025-04-13 02:08:05 +0300

    Change description

--- src/app/(home)/_components/AboutMeSection/components/Description.tsx
+++ src/app/(home)/_components/AboutMeSection/components/Description.tsx
@@ -2,14 +2,16 @@ export const Description = () => {
     return (
         <div className='p-2'>
             <p>
-                Hello, welcome to my personal website. This website about me and my articles, contents etc.
+                Hello, welcome to my personal website. This website about me and my articles and my other contents.
             </p>
             <p>
-                I'm 18 years-old fullstack developer. I can create mobile apps, websites, desktop apps and backend services. I'm interested in kotlin and web development. I like to learn new things and I love computers. I have many other hobbies like reading, writing, watching movies, and playing games.
+                I'm a 19-year-old self-taught fullstack developer. I can create mobile apps, websites, desktop apps, backend services and AI applications. I like to learn new things, listening music, playing games, reading things, drawing and so on.
             </p>
-
             <p>
-                Also, I'm not going to school and have no certificate because I think Turkey's education system is so bad and getting worse. As a citizen of Turkey, I can say school is just a waste of time for both companies and students.
+                I'm a self-taught developer who believes in real coding experience and hands-on learning. I dedicate around 80 hours per week to coding and expanding my technical knowledge.
+            </p>
+            <p>
+                I'm always open to new opportunities and collaborations. If you want to contact me, you can always find my social media accounts, email and my phone number listed above.
             </p>
         </div>
     )
