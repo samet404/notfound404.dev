200835e116a238cbdaf94497421310a44ccb5132
404 <145053429+samet404@users.noreply.github.com>
2025-05-25 22:23:19 +0300

    Update FirstPart.tsx

--- src/app/(home)/_components/AboutMeSection/components/FirstPart.tsx
+++ src/app/(home)/_components/AboutMeSection/components/FirstPart.tsx
@@ -14,7 +14,7 @@ export const FirstPart = () => {
                 sizes="calc(1.96vw + 75px)"
             />
             <div className='font-[700] text-2xl flex flex-row text-[rgba(255,255,255,0.85)] items-center gap-[0.4rem]'>
-                <div>404 / Samet Alpdeger</div>
+                <div>404 / Samet</div>
             </div>
         </div>
     )
