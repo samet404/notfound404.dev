4822034fad636f9f3602bf4020caf4e58005f40c
404 <sametalpdeger9@gmail.com>
2025-06-11 09:55:29 +0300

    Fix: bg color

--- src/app/(home)/_components/BackgroundColor.tsx
+++ src/app/(home)/_components/BackgroundColor.tsx
@@ -15,7 +15,7 @@ export const BackgroundColor = ({
                 scrollbarWidth: 'none',
             }}
             className={cn('selection:bg-[#ffffff54] bg-[hsla(201,100%,50%,1)] md:[background-image:radial-gradient(at_20%_20%,hsla(188,92%,56%,1)_0px,transparent_50%),radial-gradient(at_40%_20%,hsla(193,92%,56%,1)_0px,transparent_50%)] flex w-full flex-col gap-7 pt-[5rem] justify-center', {
-                'bg-[#006da3] [background-image:radial-gradient(at_20%_20%,hsla(193,66%,44%,1)_0px,transparent_50%),radial-gradient(at_40%_20%,hsla(200,71%,38%,1)_0px,transparent_50%)]': value
+                'bg-[#006da3] [background-image:radial-gradient(at_20%_20%,hsla(193,66%,44%,1)_0px,transparent_50%),radial-gradient(at_40%_20%,hsla(200,71%,38%,1)_0px,transparent_50%)]  md:[background-image:radial-gradient(at_20%_20%,hsla(188,87%,35%,1)_0px,transparent_50%),radial-gradient(at_40%_20%,hsla(193,78%,43%,1)_0px,transparent_50%)]': value
 
             })}
         >
