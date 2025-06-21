a0782596ccb08f8b330696542855092ad2e0133a
404 <sametalpdeger9@gmail.com>
2025-06-21 17:11:19 +0300

    Type error: Element implicitly has an 'any' type because expression of type 'any' can't be used to index type '{ 2025-5-20: string[]; 2025-5-21: string[]; }'.

--- src/app/api/get-calender-item/route.ts
+++ src/app/api/get-calender-item/route.ts
@@ -2,9 +2,9 @@
 const calenderJson = (await import('../../data/calendar.json')).default
 
 export async function POST(request: Request) {
-    const body = await request.json()
+    const body = await request.json() as { date: string }
     console.log("dsapjdasp:", body)
-    const date = calenderJson[body.date] ?? null
+    const date = calenderJson[body.date as keyof typeof calenderJson] ?? null
     console.log('date:' ,date)
     return Response.json(date)
 }
