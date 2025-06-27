4a6786bd738a61210ae09d7ac5cd617250d10b3c
404 <sametalpdeger9@gmail.com>
2025-06-27 17:34:31 +0300

    feat(article): how-to-setup-firewall-in-ubuntu.mdx

--- src/app/(article)/article/[postID]/_components/Scroll.tsx
+++ src/app/(article)/article/[postID]/_components/Scroll.tsx
@@ -1,19 +1,20 @@
-"use client"
+'use client'
 
 import useIsDarkTheme from '@/src/store/isDarkTheme'
 import type { ReactNode } from 'react'
 
 export const Scroll = ({ children }: { children: ReactNode }) => {
-    const isDarkTheme = useIsDarkTheme((state) => state.value)
+  const isDarkTheme = useIsDarkTheme((state) => state.value)
 
-    return (
-        <div
-            id='scroll-container'
-            style={{
-                scrollbarWidth: 'none',
-            }}
-            className={`w-full ${isDarkTheme ? 'bg-[#202020]' : 'bg-[#fffee8]'} pt-[7rem] pb-11 h-full selection:bg-[#0000004b] overflow-y-scroll overflow-x-hidden`}>
-            {children}
-        </div>
-    )
+  return (
+    <div
+      id="scroll-container"
+      style={{
+        scrollbarWidth: 'none',
+      }}
+      className={`w-full ${isDarkTheme ? 'bg-[#202020]' : 'bg-[#f2f0d4]'} h-full overflow-x-hidden overflow-y-scroll pb-11 pt-[7rem] selection:bg-[#0000004b]`}
+    >
+      {children}
+    </div>
+  )
 }
