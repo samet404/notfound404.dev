af5741e886aa703a83c8a26543d18f1374360d0c
404 <sametalpdeger9@gmail.com>
2025-07-07 15:03:03 +0300

    feat: new article how-to-use-ufw-with-docker

--- src/app/(article)/article/[postID]/page.tsx
+++ src/app/(article)/article/[postID]/page.tsx
@@ -1,33 +1,57 @@
-"use server"
+'use server'
 
 import { SetScrollContainer } from '@/src/components/SetScrollContainer'
 import { notFound } from 'next/navigation'
 import { Scroll } from './_components/Scroll'
 import { Prose } from './_components/Prose'
+import type { Metadata, ResolvingMetadata } from 'next'
+
+const postsJson = (await import('../../posts.json')).default
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  const { postID } = await params
+
+  // fetch data
+  const title = postsJson.find((post) => post.id === postID)?.title
+
+  return {
+    title,
+    openGraph: {
+      images: ['/images/article/' + postID],
+    },
+  }
+}
 
 const Post = async ({ params }: Props) => {
-    const { postID } = await params
+  const { postID } = await params
 
-    try {
-        const Content = (await import(`./posts/${postID}.mdx`)).default
-        return <Scroll>
-            <SetScrollContainer />
+  try {
+    const Content = (await import(`./posts/${postID}.mdx`)).default
+    return (
+      <Scroll>
+        <SetScrollContainer />
 
-            <div className='md:flex items-center justify-center'>
-                <Prose>
-                    <Content />
-                </Prose>
-            </div>
-        </Scroll>
-    } catch (error) {
-        notFound()
-    }
+        <div className="items-center justify-center md:flex">
+          <Prose>
+            <Content />
+          </Prose>
+        </div>
+      </Scroll>
+    )
+  } catch (error) {
+    notFound()
+  }
 }
 
 export default Post
 
 type Props = {
-    params: Promise<{
-        postID: string
-    }>
+  params: Promise<{
+    postID: string
+  }>
+  searchParams: Record<string, string>
 }
