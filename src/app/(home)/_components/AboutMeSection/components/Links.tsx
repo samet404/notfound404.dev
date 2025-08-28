6bff50446fc49201a71ea333e4707e8ae969ad28
404 <sametalpdeger9@gmail.com>
2025-08-28 20:40:43 +0300

    update links

--- src/app/(home)/_components/AboutMeSection/components/Links.tsx
+++ src/app/(home)/_components/AboutMeSection/components/Links.tsx
@@ -2,36 +2,66 @@ import { Svg } from '@/src/components/Svg'
 import { ProfileLink } from './ProfileLink'
 import { constants } from '@/src/constants'
 import Link from 'next/link'
+import codepen from '@/png/codepen.png'
+import Image from 'next/image'
 
 export const Links = () => {
-    return (
-        <div className='flex rounded-lg text-[0.8rem] flex-col items-center justify-center text-[white]  flex-wrap gap-4'>
-            <div className='flex flex-row flex-wrap justify-center gap-3'>
-            <div className="flex bg-[#f3be00] drop-shadow-[0_0px_4px_#f3be00d7] px-2 py-1 font-[600] rounded-md  ">
-                Open to work
-            </div>
-
-            <Link href="mailto:hi@notfound404.dev" className="flex hover:opacity-60 duration-200 bg-pink-500 drop-shadow-[0_0px_4px_#e73c99] px-2 py-1 font-[600] rounded-md  ">
-                Click to send an mail
-            </Link>
-            </div>
-
-
-            <div className='flex rounded-lg text-[0.8rem] flex-row items-center justify-center text-[white]  flex-wrap gap-3'>
-                <ProfileLink
-                    href="mailto:hi@notfound404.dev"
-                >
-                    <Svg src='mail' className='w-7 h-7' alt='email logo' />
-                </ProfileLink>
-                |
-                <ProfileLink href={`https://github.com/${constants.githubUsername}`}>
-                    <Svg src='github' className='w-6 h-6' alt='github logo' />
-                </ProfileLink>
-                |
-                <ProfileLink href="https://stackoverflow.com/users/22740544/404nnotfoundd">
-                    <Svg src='stack-overflow' className='w-7 h-7' alt='stackoverflow logo' />
-                </ProfileLink>
-            </div>
+  return (
+    <div className="flex flex-col flex-wrap items-center justify-center gap-4 rounded-lg  text-[0.8rem] text-[white]">
+      <div className="flex flex-row flex-wrap justify-center gap-3">
+        <div className="flex rounded-md bg-[#f3be00] px-2 py-1 font-[600] drop-shadow-[0_0px_4px_#f3be00d7]  ">
+          Open to work
         </div>
-    )
+
+        <Link
+          href="mailto:hi@notfound404.dev"
+          className="flex rounded-md bg-pink-500 px-2 py-1 font-[600] drop-shadow-[0_0px_4px_#e73c99] duration-200 hover:opacity-60  "
+        >
+          Click to send an mail
+        </Link>
+      </div>
+
+      <div className="flex flex-row flex-wrap items-center justify-center gap-3 px-10 rounded-lg  text-[0.8rem] text-[white]">
+        <ProfileLink href="mailto:hi@notfound404.dev">
+          <Svg src="mail" className="h-7 w-7" alt="email logo" />
+        </ProfileLink>
+        <Divider />
+        <ProfileLink href={`https://github.com/${constants.githubUsername}`}>
+          <Svg src="github" className="h-6 w-6" alt="github logo" />
+        </ProfileLink>
+        <Divider />
+        <ProfileLink href="https://stackoverflow.com/users/22740544/404nnotfoundd">
+          <Svg
+            src="stack-overflow"
+            className="h-7 w-7"
+            alt="stackoverflow logo"
+          />
+        </ProfileLink>
+        <Divider />
+        <ProfileLink href="https://codepen.io/samet404">
+          <Image src={codepen} placeholder='blur' sizes='calc(0.7vw + 26px)' className="h-7 w-7" alt="codepen" />
+        </ProfileLink>
+        <Divider />
+        <ProfileLink href="https://leetcode.com/u/samet404/">
+          <Svg
+            src="leetcode"
+            className="h-7 w-7"
+            alt="stackoverflow logo"
+          />
+        </ProfileLink>
+        <Divider />
+        <ProfileLink href="https://x.com/404nnotfounddd">
+          <Svg
+            src="xtwitter"
+            className="h-6 w-6"
+            alt="stackoverflow logo"
+          />
+        </ProfileLink>
+      </div>
+    </div>
+  )
+}
+
+const Divider = () => {
+    return <span className='hidden md:block'>|</span>
 }
