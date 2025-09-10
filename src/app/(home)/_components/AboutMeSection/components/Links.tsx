692bef028472da3e477d4492175c1e9410ead0d2
404 <sametalpdeger9@gmail.com>
2025-09-10 21:55:29 +0300

    update current state

--- src/app/(home)/_components/AboutMeSection/components/Links.tsx
+++ src/app/(home)/_components/AboutMeSection/components/Links.tsx
@@ -9,8 +9,12 @@ export const Links = () => {
   return (
     <div className="flex flex-col flex-wrap items-center justify-center gap-4 rounded-lg  text-[0.8rem] text-[white]">
       <div className="flex flex-row flex-wrap justify-center gap-3">
-        <div className="flex rounded-md bg-[#f3be00] px-2 py-1 font-[600] drop-shadow-[0_0px_4px_#f3be00d7]  ">
-          Open to work
+        {/*
+        bg-[#f3be00]
+        drop-shadow-[0_0px_4px_#f3be00d7]
+        */}
+        <div className="flex rounded-md bg-[#ef45d3d7]  px-2 py-1 font-[600]  drop-shadow-[0_0px_24px_#ff45e0d7] ">
+          Working full-time
         </div>
 
         <Link
@@ -21,7 +25,7 @@ export const Links = () => {
         </Link>
       </div>
 
-      <div className="flex flex-row flex-wrap items-center justify-center gap-3 px-10 rounded-lg  text-[0.8rem] text-[white]">
+      <div className="flex flex-row flex-wrap items-center justify-center gap-3 rounded-lg px-10  text-[0.8rem] text-[white]">
         <ProfileLink href="mailto:hi@notfound404.dev">
           <Svg src="mail" className="h-7 w-7" alt="email logo" />
         </ProfileLink>
@@ -39,31 +43,25 @@ export const Links = () => {
         </ProfileLink>
         <Divider />
         <ProfileLink href="https://codepen.io/samet404">
-          <Image src={codepen} placeholder='blur' sizes='calc(0.7vw + 26px)' className="h-7 w-7" alt="codepen" />
+          <Image
+            src={codepen}
+            placeholder="blur"
+            sizes="calc(0.7vw + 26px)"
+            className="h-7 w-7"
+            alt="codepen"
+          />
         </ProfileLink>
         <Divider />
         <ProfileLink href="https://leetcode.com/u/samet404/">
-          <Svg
-            src="leetcode"
-            className="h-7 w-7"
-            alt="stackoverflow logo"
-          />
+          <Svg src="leetcode" className="h-7 w-7" alt="stackoverflow logo" />
         </ProfileLink>
         <Divider />
         <ProfileLink href="https://x.com/404nnotfounddd">
-          <Svg
-            src="xtwitter"
-            className="h-6 w-6"
-            alt="stackoverflow logo"
-          />
+          <Svg src="xtwitter" className="h-6 w-6" alt="stackoverflow logo" />
         </ProfileLink>
         <Divider />
         <ProfileLink href="https://www.youtube.com/@404nnotfoundd">
-          <Svg
-            src="youtube"
-            className="h-8 w-8"
-            alt="youtube logo"
-          />
+          <Svg src="youtube" className="h-8 w-8" alt="youtube logo" />
         </ProfileLink>
       </div>
     </div>
@@ -71,5 +69,5 @@ export const Links = () => {
 }
 
 const Divider = () => {
-    return <span className='hidden md:block'>|</span>
+  return <span className="hidden md:block">|</span>
 }
