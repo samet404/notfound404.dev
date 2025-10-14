9aa5029b156d2690ff029a9d95f19670be9b59e6
404 <sametalpdeger9@gmail.com>
2025-10-14 20:37:24 +0300

    Fix: Type error: Module '..' has no exported member 'Prose'.

--- src/app/(home)/_components/AboutMeSection/components/Experiences.tsx
+++ src/app/(home)/_components/AboutMeSection/components/Experiences.tsx
@@ -1,21 +1,4 @@
-import { Prose } from '..'
 
 export const Experiences = () => {
-  return (
-    <div className="relative flex h-[20rem] w-full flex-col items-center">
-      <div
-        style={{
-          backgroundColor: 'hsla(272,88%,60%,1)',
-          backgroundImage:
-            'radial-gradient(at 0% 100%, hsla(303,100%,39%,1) 0px, transparent 50%),radial-gradient(at 87% 0%, hsla(198,77%,29%,1) 0px, transparent 50%)',
-          transform: 'rotate(-5deg)',
-        }}
-        className="absolute left-[-50vw] top-[0] z-[0] h-full w-[200vw]"
-      ></div>
-
-      <div className="z-10 w-full">
-        
-      </div>
-    </div>
-  )
+  return <></>
 }
